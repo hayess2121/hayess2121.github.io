bad49d008db01b489b7ffa2c2bc98612b841729e
hayess2121 <hayess2121@gmail.com>
2026-03-24 17:08:31 +0000

    i did the dumb ah filtering

--- asd-projects/image-filtering/index.js
+++ asd-projects/image-filtering/index.js
@@ -20,8 +20,11 @@ function resetAndRender() {
 // all of your apply functions
 function applyAndRender() {
   // Multiple TODOs: Call your apply function(s) here
-
-  
+  applyFilter(reddify);
+  applyFilterNoBackground(decreaseBlue);
+  applyFilter(increaseGreenByBlue);
+  applyFilterNoBackground(reddify);
+ 
 
   // do not change the below line of code
   render($("#display"), image);
@@ -32,18 +35,74 @@ function applyAndRender() {
 /////////////////////////////////////////////////////////
 
 // TODO 1, 2, 3 & 5: Create the applyFilter function here
-
+function applyFilter(filterFunction)
+{
+for (let i = 0; i < image.length; i++)
+{
+  for (let j = 0; j < image[i].length; j++)
+  {
+    let pixel = image[i][j]
+    let pixelArray = rgbStringToArray(pixel)
+    //
+    filterFunction(pixelArray)
+    let updatedPixel = rgbArrayToString(pixelArray)
+    image[i][j] = updatedPixel
+    console.log(image[i][j])
+  }
+}
+}
 
 // TODO 9 Create the applyFilterNoBackground function
+function applyFilterNoBackground(filterFunction)
+{
+  var backGroundColor = image[0][0]
+  for (let i = 0; i < image.length; i++)
+  {
+    for (let j = 0; j < image[i].length; j++)
+    {
+      if (image[i][j] !== image[0][0])
+      {
+      image[i][j] = keepInBounds(rgbArrayToString(filterFunction(rgbStringToArray(image[i][j]))))
+      }
+    }
+  }
 
+}
 
 // TODO 6: Create the keepInBounds function
-
-
+function keepInBounds(number)
+{
+if(number < 0)
+{
+  return 0
+}
+else if(number > 255)
+{
+  return 255
+}
+else
+{
+  return number
+}
+}
 // TODO 4: Create reddify filter function
-
+function reddify(pixelArray)
+{
+pixelArray[RED] = 255
+return pixelArray[RED]
+}
 
 // TODO 7 & 8: Create more filter functions
-
-
+function decreaseBlue(pixelArray)
+{
+pixelArray[BLUE] = pixelArray[BLUE] - 50
+pixelArray[BLUE] = keepInBounds(pixelArray[BLUE])
+return pixelArray
+}
+function increaseGreenByBlue(pixelArray)
+{
+  pixelArray[GREEN] += pixelArray[BLUE]
+  pixelArray[GREEN] = keepInBounds(pixelArray[GREEN])
+  return pixelArray
+}
 // CHALLENGE code goes below here
